19393fdc791cf0190dfd3307bf78cdc719720ebe
gabrieldev525 <gvictor525.gv@gmail.com>
2020-02-01 12:22:57 -0300

    init project

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Feed from './src/containers/feed'
+
+const App = () => {
+  return (
+    <Feed />
+  );
+};
+
+export default App;
---
+++ src/containers/feed.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import StorySession from '../components/story'
+import {
+  MainView
+} from '../components/common/style'
+
+export const Feed = () => {
+  return (
+    <MainView>
+      <StorySession />
+    </MainView>
+  )
+}
+
+export default Feed
